9d419e89f75ac7260c8e905789150421931b3f5c
Christin <christinwhite1@icloud.com>
2024-05-07 14:49:28 -0400

    Main - fix while loop

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
-
 public class TicTacToe {
+
     public static void main(String[] args) {
+        //scanner class
         Scanner scanner = new Scanner(System.in);
+
+        //Creating objects
         Board board = new Board();
         Player playerX = new Player('X');
         Player playerO = new Player('O');
@@ -10,11 +13,18 @@ public class TicTacToe {
 
         while (true) {
             board.printBoard();
+
             Player currentPlayer = game.getCurrentPlayer();
-            System.out.println("Player " + currentPlayer.getSymbol() + "'s turn. Enter row and column (0-2):");
+            System.out.println("Player " + currentPlayer.symbol() + "'s turn. Enter row and column (0-2):");
+
             int row = scanner.nextInt();
             int col = scanner.nextInt();
             game.makeMove(row, col);
+
+
         }
     }
 }
+
+
+
